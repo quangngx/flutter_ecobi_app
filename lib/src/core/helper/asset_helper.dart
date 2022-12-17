3a88b10916b37cd746fe71ddfec741ffba8fa760
Quang Nguyễn <2jiro.jp@gmail.com>
2022-12-17 12:59:48 +0700

    icon init

--- lib/src/core/helper/asset_helper.dart
+++ lib/src/core/helper/asset_helper.dart
@@ -4,6 +4,74 @@ mixin AssetHelper {
   static const String icoProfile = 'assets/icon/ico_profile.png';
   static const String icoWelcome = 'assets/icon/ico_welcome.png';
 
+  static const String icoAdd = 'assets/icon/line/ico_add.png';
+  static const String icoArchive = 'assets/icon/line/ico_archive.png';
+  static const String icoArrowLeft = 'assets/icon/line/ico_arrow_left.png';
+  static const String icoArrowRight = 'assets/icon/line/ico_arrow_right.png';
+  static const String icoBagTimer = 'assets/icon/line/ico_bag_timer.png';
+  static const String icoBag = 'assets/icon/line/ico_bag.png';
+  static const String icoCamera = 'assets/icon/line/ico_camera.png';
+  static const String icoCards = 'assets/icon/line/ico_cards.png';
+  static const String icoCloseCircle = 'assets/icon/line/ico_close_circle.png';
+  static const String icoClose = 'assets/icon/line/ico_close.png';
+  static const String icoDetails = 'assets/icon/line/ico_details.png';
+  static const String icoEdit = 'assets/icon/line/ico_edit.png';
+  static const String icoFilter = 'assets/icon/line/ico_filter.png';
+  static const String icoFlash = 'assets/icon/line/ico_flash.png';
+  static const String icoHeart = 'assets/icon/line/ico_heart.png';
+  static const String icoHome = 'assets/icon/line/ico_home.png';
+  static const String icoMultiply = 'assets/icon/line/ico_multiply.png';
+  static const String icoProfileLine = 'assets/icon/line/ico_profile.png';
+  static const String icoDiscount =
+      'assets/icon/line/ico_receipt_disscount.png';
+  static const String icoScan = 'assets/icon/line/ico_scan.png';
+  static const String icoSearch = 'assets/icon/line/ico_search_normal.png';
+  static const String icoSecurity = 'assets/icon/line/ico_security_user.png';
+  static const String icoSetting = 'assets/icon/line/ico_settings.png';
+  static const String icoTagCross = 'assets/icon/line/ico_tag_cross.png';
+  static const String icoTicket = 'assets/icon/line/ico_ticket.png';
+  static const String icoTrash = 'assets/icon/line/ico_trash.png';
+  static const String icoTrend = 'assets/icon/line/ico_trend.png';
+
+  static const String icoBoldAddSquare = 'assets/icon/bold/ico_add_square.png';
+  static const String icoBoldAdd = 'assets/icon/bold/ico_add.png';
+  static const String icoBoldArchive = 'assets/icon/bold/ico_archive.png';
+  static const String icoBoldbagTimer = 'assets/icon/bold/ico_bag_timer.png';
+  static const String icoBoldBag = 'assets/icon/bold/ico_bag.png';
+  static const String icoBoldCall = 'assets/icon/bold/ico_call.png';
+  static const String icoBoldCamera = 'assets/icon/bold/ico_camera.png';
+  static const String icoBoldCardSecure =
+      'assets/icon/bold/ico_card_secured.png';
+  static const String icoBoldCard = 'assets/icon/bold/ico_card.png';
+  static const String icoBoldClock = 'assets/icon/bold/ico_clock.png';
+  static const String icoBoldClose = 'assets/icon/bold/ico_close.png';
+  static const String icoBoldDirection = 'assets/icon/bold/ico_directrion.png';
+  static const String icoBoldEdit = 'assets/icon/bold/ico_edit.png';
+  static const String icoBoldFilterTick =
+      'assets/icon/bold/ico_filter_tick.png';
+  static const String icoBoldFilter = 'assets/icon/bold/ico_filter.png';
+  static const String icoBoldFlash = 'assets/icon/bold/ico_flash.png';
+  static const String icoBoldHome = 'assets/icon/bold/ico_home.png';
+  static const String icoBoldCircle = 'assets/icon/bold/ico_info_circle.png';
+  static const String icoBoldLocation = 'assets/icon/bold/ico_location.png';
+  static const String icoBoldLock = 'assets/icon/bold/ico_lock.png';
+  static const String icoBoldMinus = 'assets/icon/bold/ico_minus.png';
+  static const String icoBoldPeople = 'assets/icon/bold/ico_people.png';
+  static const String icoBoldProfile = 'assets/icon/bold/ico_profile.png';
+  static const String icoBoldReceiptDiscount =
+      'assets/icon/bold/ico_receipt_disscount.png';
+  static const String icoBoldSearch = 'assets/icon/bold/ico_search_normal.png';
+  static const String icoBoldSecutiryUser =
+      'assets/icon/bold/ico_security_user.png';
+  static const String icoBoldSetting = 'assets/icon/bold/ico_setting.png';
+  static const String icoBoldSms = 'assets/icon/bold/ico_sms.png';
+  static const String icoBoldStar = 'assets/icon/bold/ico_star.png';
+  static const String icoBoldTicketDiscount =
+      'assets/icon/bold/ico_ticket_discount.png';
+  static const String icoBoldTrash = 'assets/icon/bold/ico_trash.png';
+  static const String icoBoldUnsaved = 'assets/icon/bold/ico_unsaved.png';
+  static const String icoBoldWarning = 'assets/icon/bold/ico_warning.png';
+
   //Assets in images
   static const String slide1 = 'assets/slide1.png';
   static const String slide2 = 'assets/slide2.png';
@@ -14,4 +82,15 @@ mixin AssetHelper {
   static const String bgScroll = 'assets/bg_scroll.png';
   static const String bgWelcome = 'assets/bg_welcome.png';
   static const String bgWelcome2 = 'assets/bg_welcome_2.png';
+  static const String bgDelete = 'assets/bg_delete.png';
+  static const String bgEmptyCard = 'assets/bg_empty_card.png';
+  static const String bgPayment = 'assets/bg_payment_success.png';
+  static const String bgCamera = 'assets/bg_camera_permission.png';
+
+  static const String bgAvocados = 'assets/avocados.png';
+  static const String bgBeefRibs = 'assets/beef_ribs.png';
+  static const String bgCoffee = 'assets/coffee.png';
+  static const String bgCorn = 'assets/corn.png';
+  static const String bgMilk = 'assets/milk.png';
+  static const String bgSalmon = 'assets/salmon.png';
 }
--- lib/src/views/home_page.dart
+++ lib/src/views/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecobi_app/src/core/helper/helper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,6 +7,16 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Row(
+            children: [
+              
+            ],
+          )
+        ]),
+      ),
+    );
   }
 }
