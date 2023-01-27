58ce3467e94535561d63439f8b58f37c2826c082
Quang Nguyễn <2jiro.jp@gmail.com>
2023-01-27 16:20:34 +0700

    show snack bar

--- lib/src/widgets/product_item.dart
+++ lib/src/widgets/product_item.dart
@@ -40,6 +40,15 @@ class ProductItem extends StatelessWidget {
             ),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: const Text('Added to cart'),
+                duration: const Duration(seconds: 1),
+                action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    }),
+              ));
             },
           ),
           title: Text(
